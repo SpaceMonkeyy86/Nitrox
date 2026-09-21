0ea40dfb79677c624a6475411d4e48061d829e44
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2026-09-21 18:16:20 -0400

    Fix internal leak points

---
+++ NitroxPatcher/Patches/Dynamic/CyclopsExternalDamageManager_UpdateOvershield_Patch.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace NitroxPatcher.Patches.Dynamic;
+
+// Fixes a very annoying vanilla game bug where the Cyclops will appear
+// to be leaking from the inside even though there is no actual damage.
+public sealed partial class CyclopsExternalDamageManager_UpdateOvershield_Patch : NitroxPatch, IDynamicPatch
+{
+    public static readonly MethodInfo TARGET_METHOD = Reflect.Method((CyclopsExternalDamageManager t) => t.UpdateOvershield());
+
+    public static void Postfix(CyclopsExternalDamageManager __instance)
+    {
+        __instance.ToggleLeakPointsBasedOnDamage();
+    }
+}
